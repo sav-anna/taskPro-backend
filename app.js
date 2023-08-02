1ca9b614b2b8aab1567819509a7807fa06b267ce
Anna Sibova <anna_sibova@ukr.net>
2023-08-02 22:11:07 +0300

    start

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const logger = require("morgan");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 require("dotenv").config();
 
@@ -18,6 +20,8 @@ app.use(express.static("public"));
 
 app.use("/api/auth", authRouter);
 // app.use("/api/contacts", contactsRouter);
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use((req, res) => {
   res.status(404).json({ message: "Not found" });

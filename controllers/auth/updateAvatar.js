2107ca51c5a77b3e184fc1c3feeff7b420cfa145
chemvic <chemvic79@gmail.com>
2023-08-03 11:48:43 +0300

    добавил удаление предыдущей аватарки из public

--- controllers/auth/updateAvatar.js
+++ controllers/auth/updateAvatar.js
@@ -3,26 +3,56 @@ const fs = require("fs/promises");
 const Jimp = require("jimp");
 const { User } = require("../../models/user");
 
-const avatarsDir = path.join(__dirname, "../../", "public", "avatars");
+const { HttpError } = require('../../helpers');
 
-const updateAvatar = async (req, res) => {
-  const { _id } = req.user;
-  const { path: tempUpload, originalname } = req.file;
-  const fileName = `${_id}_${originalname}`;
-  const resultUpload = path.join(avatarsDir, fileName);
+const avatarsDir = path.join(__dirname, '../../', 'public', 'avatars');
 
-  await Jimp.read(tempUpload)
-    .then((image) => {
-      return image.resize(250, 250).quality(60).greyscale().write(resultUpload);
-    })
-    .catch((err) => {
-      throw new Error(`Failed to resize avatar: ${err}`);
-    });
-  await fs.unlink(tempUpload);
-  //   await fs.rename(tempUpload, resultUpload); якщо без Jimpt
-  const avatarURL = path.join("avatars", fileName);
-  await User.findByIdAndUpdate(_id, { avatarURL });
-  res.json({ avatarURL });
+const updateAvatar = async(req, res, next) =>{
+     const {path: tempUpload, originalname} = req.file;
+     try {
+     const image = await Jimp.read(tempUpload);
+     await image
+     .autocrop()
+     .cover(
+        250,250,Jimp.HORIZONTAL_ALIGN_CENTER ||Jimp.VERTICAL_ALIGN_MIDDLE
+     )
+      .writeAsync(tempUpload);
+     } catch (error) {
+        console.error(error);
+     }
+
+     const {_id} = req.user;
+     const avatarName = `${_id}_${originalname}`;
+    try {  
+      if(tempUpload ===""){return}; 
+        const resultUpload = path.join(avatarsDir, avatarName);
+        const prevAvatarURL = await User.findById(_id).select('avatarURL');       
+        const prevAvatarPath = path.join(__dirname,'../../', prevAvatarURL.avatarURL);
+         
+        // удаление старой аватарки из папки public при обновлении
+            try {
+                const stats = await fs.stat(prevAvatarPath);
+                if (stats.isFile()) {
+                    await fs.unlink(prevAvatarPath);
+                }
+            } catch (err) {
+                if (err.code !== 'ENOENT') {
+                    console.log("ошибка удаления старой аватарки: ", err);
+                }
+            };           
+            
+        
+        await fs.rename(tempUpload, resultUpload);
+        const avatarURL = path.join('public', 'avatars', avatarName);
+
+        await User.findByIdAndUpdate(_id, {avatarURL});
+        res.json({avatarURL});       
+    } catch (error) {
+       await fs.unlink(tempUpload);
+       next(HttpError(401, 'Not authorized'));
+    }
+
 };
 
 module.exports = updateAvatar;
+

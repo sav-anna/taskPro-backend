bb1d6250cc8a91110ba56eec3b18e0d95011f41a
chemvic <chemvic79@gmail.com>
2023-08-06 12:42:55 +0300

    upd

--- helpers/authHelper.js
+++ helpers/authHelper.js
@@ -1,51 +1,112 @@
+// const {SECRET_KEY}= process.env;
+// const {tokens} = require('../config/app').jwt;
+// const nanoid= require('nanoid');
+// const jwt = require("jsonwebtoken");
+// const Token = require('../models/token');
+
+// const generateAccessToken = (userId) =>{
+//     const payload ={
+//         userId,
+//         type: tokens.access.type,
+//     };
+//     const options = {
+//         expiresIn: tokens.access.expiresIn,
+//     };
+//     return jwt.sign(payload, SECRET_KEY, options);
+// };
+// const generateRefreshToken = () =>{
+//     const payload ={
+//         id: nanoid(),
+//         type: tokens.refresh.type,
+//     };
+//     const options = {
+//         expiresIn: tokens.refresh.expiresIn,
+//     };
+//     return {
+//         id: payload.id,
+//         token: jwt.sign(payload, SECRET_KEY, options),
+//     };
+// };
+// const replaceDbRefreshToken = async(tokenId, userId) =>{
+//     await Token.findByIdAndDelete(userId);
+
+//     const result = await Token.create({tokenId, userId});
+//     return result;
+// };
+
+// const updateTokens =async(userId)=>{
+//     const accessToken = generateAccessToken(userId);
+//     const refreshToken = generateRefreshToken();
+//     await replaceDbRefreshToken(userId, refreshToken.id);
+
+//     return {accessToken, refreshToken: refreshToken.token};
+// };
+
+//  const authHelper ={
+// generateAccessToken,
+// generateRefreshToken,
+// replaceDbRefreshToken,
+// updateTokens
+// };
+// module.exports = authHelper;
+
 const {SECRET_KEY}= process.env;
 const {tokens} = require('../config/app').jwt;
 const nanoid= require('nanoid');
 const jwt = require("jsonwebtoken");
 const Token = require('../models/token');
 
-const generateAccessToken = (userId) =>{
-    const payload ={
-        userId,
-        type: tokens.access.type,
-    };
-    const options = {
-        expiresIn: tokens.access.expiresIn,
-    };
-    return jwt.sign(payload, SECRET_KEY, options);
-};
-const generateRefreshToken = () =>{
-    const payload ={
-        id: nanoid(),
-        type: tokens.refresh.type,
-    };
-    const options = {
-        expiresIn: tokens.refresh.expiresIn,
-    };
-    return {
-        id: payload.id,
-        token: jwt.sign(payload, SECRET_KEY, options),
-    };
-};
-const replaceDbRefreshToken = async(tokenId, userId) =>{
-    await Token.findByIdAndDelete(userId);
+const generateAccessToken = (userId) => {
+  const payload = {
+    userId,
+    type: tokens.access.type,
+  }
 
-    const result = await Token.create({tokenId, userId});
-    return result;
-};
+  const options = {
+    expiresIn: tokens.access.expiresIn,
+  }
 
-const updateTokens =async(userId)=>{
-    const accessToken = generateAccessToken(userId);
-    const refreshToken = generateRefreshToken();
-    await replaceDbRefreshToken(userId, refreshToken.id);
+  return jwt.sign(payload, SECRET_KEY, options)
+}
 
-    return {accessToken, refreshToken: refreshToken.token};
-};
+const generateRefreshToken = () => {
+  const payload = {
+    id: nanoid(),
+    type: tokens.refresh.type,
+  }
 
- const authHelper ={
-generateAccessToken,
-generateRefreshToken,
-replaceDbRefreshToken,
-updateTokens
-};
-module.exports = authHelper;
+  const options = {
+    expiresIn: tokens.refresh.expiresIn,
+  }
+
+  return {
+    id: payload.id,
+    token: jwt.sign(payload, SECRET_KEY, options),
+  }
+}
+
+const replaceDbRefreshToken = async (tokenId, userId) => {
+  await Token.findByIdAndDelete(userId)
+
+  const result = await Token.create({ tokenId, userId })
+
+  return result
+}
+
+const updateTokens = async (userId) => {
+  const accessToken = authHelper.generateAccessToken(userId)
+  const refreshToken = authHelper.generateRefreshToken()
+
+  await authHelper.replaceDbRefreshToken(refreshToken.id, userId)
+
+  return { accessToken, refreshToken: refreshToken.token }
+}
+
+const authHelper = {
+  generateAccessToken,
+  generateRefreshToken,
+  replaceDbRefreshToken,
+  updateTokens,
+}
+
+module.exports = authHelper
--- models/token.js
+++ models/token.js
@@ -1,37 +1,53 @@
-const { Schema, model } = require("mongoose");
-const { handleMongooseError } = require("../helpers");
-// const Joi = require("joi");
+// const { Schema, model } = require("mongoose");
+// const { handleMongooseError } = require("../helpers");
+// // const Joi = require("joi");
 
-// const emailRegexp = /^[a-zA-Z0-9.!#$%&’*+/=?^_{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/;
-// const nameRegexp =/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
-// const passwordRegexp = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+// // const emailRegexp = /^[a-zA-Z0-9.!#$%&’*+/=?^_{|}~-]+@[a-zA-Z0-9-]+(?:.[a-zA-Z0-9-]+)*$/;
+// // const nameRegexp =/^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+// // const passwordRegexp = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
 
 
-const tokenSchema = new Schema(
-  {
-    tokenId: {
-      type: String,
-    //   required: true,
-    },
-    userId: {
-      type: String,
-    //   required: true,
-    },
+// const tokenSchema = new Schema(
+//   {
+//     tokenId: {
+//       type: String,
+//     //   required: true,
+//     },
+//     userId: {
+//       type: String,
+//     //   required: true,
+//     },
     
+//   },
+//   { versionKey: false, timestamps: true }
+// );
+
+// tokenSchema.post("save", handleMongooseError);
+
+
+// // const loginSchema = Joi.object({
+// //   email: Joi.string().pattern(emailRegexp).required(),
+// //   password: Joi.string().pattern(passwordRegexp).min(8).max(64).required(),
+// // });
+
+
+
+// const Token = model("token", tokenSchema);
+
+// module.exports = Token;
+const { Schema, model } = require('mongoose')
+
+const schema = new Schema(
+  {
+    tokenId: String,
+    userId: String,
   },
-  { versionKey: false, timestamps: true }
-);
+  {
+    timestamps: false,
+    versionKey: false,
+  }
+)
 
-tokenSchema.post("save", handleMongooseError);
+const Token = model('token', schema)
 
-
-// const loginSchema = Joi.object({
-//   email: Joi.string().pattern(emailRegexp).required(),
-//   password: Joi.string().pattern(passwordRegexp).min(8).max(64).required(),
-// });
-
-
-
-const Token = model("token", tokenSchema);
-
-module.exports = Token;
+module.exports = Token

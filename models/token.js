fd03938df75801314f714074e3f5689787e73f5e
chemvic <chemvic79@gmail.com>
2023-08-07 18:12:34 +0300

    Update token.js

--- models/token.js
+++ models/token.js
@@ -20,20 +20,3 @@ const tokenSchema = new Schema(
 const Token = model("token", tokenSchema);
 
 module.exports = Token;
-
-// const { Schema, model } = require('mongoose')
-
-// const schema = new Schema(
-//   {
-//     tokenId: String,
-//     userId: String,
-//   },
-//   {
-//     timestamps: false,
-//     versionKey: false,
-//   }
-// )
-
-// const Token = model('token', schema)
-
-// module.exports = Token

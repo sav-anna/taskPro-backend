09b37afc7a662ce7da200784c5585e8091c2a764
chemvic <chemvic79@gmail.com>
2023-08-11 19:29:26 +0300

    Update logout.js

--- controllers/auth/logout.js
+++ controllers/auth/logout.js
@@ -3,6 +3,6 @@ const logout = async (req, res) => {
   const { user } = req;
 
   await Token.findOneAndRemove({ userId: user._id });
-  res.status(204);
+  res.status(200).json({message:'Logout successful'});
 };
 module.exports = logout;

38c0f292c749f67dee78c5a72b0c3a56fac44205
Tetiana Ruzhetska <tetiana.ruzhetska@gmail.com>
2023-08-14 23:51:47 +0300

    fixed

--- controllers/auth/googleAuth.js
+++ controllers/auth/googleAuth.js
@@ -1,31 +1,19 @@
-// const bcrypt = require("bcrypt");
+const bcrypt = require("bcrypt");
 const { User } = require("../../models/user");
 const { HttpError, authHelper } = require("../../helpers");
 
 const { FRONTEND_URL } = process.env;
 
 const googleAuth = async (req, res) => {
-  const { _id: id, name, email, theme, avatarURL } = req.user;
+  const { _id: id } = req.user;
 
   const { accessToken, refreshToken } = await authHelper.updateTokens(id);
 
   await User.findByIdAndUpdate(id, { accessToken, refreshToken });
 
-  res.json({
-    id,
-    token: accessToken,
-    refreshToken,
-    theme,
-    avatarURL,
-    name,
-    email,
-  });
-
-  res.redirect(`${FRONTEND_URL}/auth/login`);
-
-  // res.redirect(
-  //   `${FRONTEND_URL}/auth/login?accessToken=${accessToken}&refreshToken=${refreshToken}`
-  // );
+  res.redirect(
+    `${FRONTEND_URL}/auth/login?accessToken=${accessToken}&refreshToken=${refreshToken}`
+  );
   // res.redirect(`${FRONTEND_URL}/home`);
   //   res.redirect(`${FRONTEND_URL}?accessToken=${accessToken}&refreshToken=${refreshToken}`)
 };
